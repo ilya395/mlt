5d96aae3e296af4a257131d40cc3cef50826ded5
ilya395 <ilya395.efimenko@yandex.ru>
2020-07-08 17:14:36 +0300

    v_11.1

---
+++ dist/assets/js/common.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    const topMenu = new Vue({
+        el: "#top-menu",
+        data: {
+            isActive: false,
+        },
+        methods: {
+            visibleElementInHeader: function () { // v-on:click="isActive = !isActive"
+                this.isActive = !this.isActive;
+                //
+                const elemInHeader = document.querySelector('header');
+                elemInHeader.classList.contains('active') 
+                    ? elemInHeader.classList.remove('active') 
+                    : elemInHeader.classList.add('active');
+                //
+                const body = document.querySelector('body');
+                body.classList.contains('hidden')
+                    ? body.classList.remove('hidden')
+                    : body.classList.add('hidden');
+            }
+        }
+    });
+});
--- src/assets/js/common_def.js/common.js
+++ src/assets/js/common_def.js/common.js
@@ -1,1 +1,22 @@
 "use strict";
+
+window.addEventListener('load', function () {
+  var topMenu = new Vue({
+    el: "#top-menu",
+    data: {
+      isActive: false
+    },
+    methods: {
+      visibleElementInHeader: function visibleElementInHeader() {
+        // v-on:click="isActive = !isActive"
+        this.isActive = !this.isActive; //
+
+        var elemInHeader = document.querySelector('header');
+        elemInHeader.classList.contains('active') ? elemInHeader.classList.remove('active') : elemInHeader.classList.add('active'); //
+
+        var body = document.querySelector('body');
+        body.classList.contains('hidden') ? body.classList.remove('hidden') : body.classList.add('hidden');
+      }
+    }
+  });
+});
